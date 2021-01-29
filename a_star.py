c3101effad157b2c38cadf0577d8dc940342ee08
megamxl <maximilian.nowak@outlook.at>
2021-01-29 07:09:08 +0100

    Borders are now visiual

--- a_star.py
+++ a_star.py
@@ -6,6 +6,7 @@ import pygame
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
+GREY = (112,128,144)
 RED = (255, 0, 0)
  
 # This sets the WIDTH and HEIGHT of each grid location
@@ -18,23 +19,24 @@ MARGIN = 5
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
 grid = []
-for row in range(10):
+for row in range(28):
     # Add an empty array that will hold each cell
     # in this row
     grid.append([])
-    for column in range(10):
+    for column in range(51):
         grid[row].append(0)  # Append a cell
  
- 
+border_grid =[]
+
 # Initialize pygame
 pygame.init()
  
 # Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [255, 255]
+WINDOW_SIZE = [1280,720]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
-pygame.display.set_caption("Array Backed Grid")
+pygame.display.set_caption("a Star visualisation")
  
 # Loop until the user clicks the close button.
 done = False
@@ -44,6 +46,7 @@ done = False
 clock = pygame.time.Clock()
 
 start_color = True
+end_color = True
 
 # -------- Main Program Loop -----------
 while not done:
@@ -63,25 +66,31 @@ while not done:
                 print("startpint ", startpoint_x, startpoint_y)
                 grid[row][column] = 1
                 start_color = False
-            else:
+            elif end_color == True:
                 endpoint_x= row
                 endpoint_y = column
                 print("endpiont", endpoint_x, endpoint_y)
                 grid[row][column] = 2
-            print("Click ", pos, "Grid coordinates: ", row, column)
+                end_color = False
+            else:
+                border_grid.append([row,column])
+                grid[row][column] = 3
+                print("Click ", pos, "Grid coordinates: ", row, column)
  
     # Set the screen background
     screen.fill(BLACK)
  
     # Draw the grid
     
-    for row in range(10):
-        for column in range(10):
+    for row in range(28):
+        for column in range(51):
             color = WHITE
             if grid[row][column] == 1:
                     color = GREEN
             if grid[row][column] == 2:
                     color = RED
+            if grid[row][column] == 3:
+                color = GREY
             pygame.draw.rect(screen,
                             color,
                             [(MARGIN + WIDTH) * column + MARGIN,
