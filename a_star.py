06de01544baa73fa41b2a9bf4a37d9f6d6693aa6
megamxl <maximilian.nowak@outlook.at>
2021-02-23 09:38:52 +0100

    Signed-off-by: megamxl <maximilian.nowak@outlook.at>

--- a_star.py
+++ a_star.py
@@ -16,6 +16,21 @@ HEIGHT = 20
  
 # This sets the margin between each cell
 MARGIN = 5
+
+class Node():
+    """A node class for A* Pathfinding"""
+
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
  
 # Create a 2 dimensional array. A two dimensional
 # array is simply a list of lists.
@@ -58,8 +73,91 @@ def aStarbegin(border_grid,start_x,start_y,end_x,end_y,currentpoint):
     if is_bloked(border_grid,start_arry) or is_bloked(border_grid,end_array):
         print("point is blocked")
         return 
-    arr = [[0 for i in range(cols)] for j in range(rows)]   
+    #arr = [[0 for i in range(cols)] for j in range(rows)]   
     
+def a_star(maze,start_x, start_y,end_x, end_y):
+    start = (start_x, start_y)
+    end = (end_x, end_y)
+
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    while len(open_list) > 0:
+    
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+
+    
+
 grid = []
 for row in range(28):
     # Add an empty array that will hold each cell
@@ -70,7 +168,14 @@ for row in range(28):
 
 grid_clean = grid
 
-border_grid =[]
+border_grid = []
+for row in range(28):
+    # Add an empty array that will hold each cell
+    # in this row
+    border_grid.append([])
+    for column in range(51):
+        border_grid[row].append(0)  # Append a cell
+
 
 # Initialize pygame
 pygame.init()
@@ -117,8 +222,11 @@ while not done:
                 grid[row][column] = 2
                 end_color = False
             else:
-                border_grid.append([row,column])
+                #border_grid.append([row,column])
+                border_grid[row][column] = 1
                 grid[row][column] = 3
+                path = a_star(border_grid,startpoint_x,startpoint_y, endpoint_x, endpoint_y)
+                print (path)
                 print("Click ", pos, "Grid coordinates: ", row, column)
  
     # Set the screen background
@@ -135,7 +243,7 @@ while not done:
                     color = RED
             if grid[row][column] == 3:
                 color = GREY
-                aStarbegin(border_grid,startpoint_x,startpoint_y,endpoint_x,endpoint_y)
+                aStarbegin(border_grid,startpoint_x,startpoint_y,endpoint_x,endpoint_y,border_grid[len(border_grid)-1])
             pygame.draw.rect(screen,
                             color,
                             [(MARGIN + WIDTH) * column + MARGIN,
